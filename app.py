5c1808f79fffc308619d30c5585acb8b5a6ca4f5
metalerk <rodriguezjluis0@gmail.com>
2017-05-18 15:29:21 -0500

    test travis 5

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ class Operaciones:
     @staticmethod
     def suma(num1, num2):
         return num1 + num2
+
+    @staticmethod
+    def resta(num1, num2):
+        return num1 - num2
--- test.py
+++ test.py
@@ -6,3 +6,8 @@ class TestOperaciones(unittest.TestCase):
         num1 = 2
         num2 = 4
         self.assertEqual(Operaciones.suma(num1, num2), 6)
+
+    def test_resta(self):
+        num1 = 34
+        num2 = 24
+        self.assertEqual(Operaciones.resta(num1, num2), 10)
